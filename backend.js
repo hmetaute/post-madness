a6b6993fad23213b2d8db2baed099473b715bafc
Hernan Dario Metaute Sarmiento <hernan.metaute@ceiba.com.co>
2014-05-19 22:21:16 -0500

    heroku config files and log dependency

---
+++ backend.js
@@ -0,0 +1,17 @@
+// Server setup
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+//configuration
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res){
+	res.send('Hello, World!');
+    });
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function(){
+	console.log("Listening on " + port);
+    });
