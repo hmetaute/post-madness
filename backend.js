07505a78a58721d685dafa5a4ea2e19d5667f098
Hernan Dario Metaute Sarmiento <hernan.metaute@ceiba.com.co>
2014-05-19 23:17:31 -0500

    showing off

--- backend.js
+++ backend.js
@@ -9,7 +9,7 @@ app.use(logfmt.requestLogger());
 
 
 app.get('/saludo-laura', function(req, res){
-	res.send('<h1>Hola, Laura mi hermosa. Esto es lo que estoy haciendo haciendo. Pense que era mas facil que me entendieras si te mostraba! :* </h1> Una imagencita para ti: <img src="http://sp1.fotolog.com/photo/33/49/30/simpsonss1/1244121697099_f.jpg">');
+	res.send('<h1>Hola, Laura mi hermosa. Esto es lo que estoy haciendo haciendo. Pense que era mas facil que me entendieras si te mostraba! :* </h1> Una imagencita para ti: <img src="http://sp1.fotolog.com/photo/33/49/30/simpsonss1/1244121697099_f.jpg"> <b> y un videito: <iframe width="560" height="315" src="//www.youtube.com/embed/w_hdJU-tK8o" frameborder="0" allowfullscreen></iframe>');
     });
 
 app.get('/', function(req, res){
